6e2d392f28eed03f4db1495e28050f2e2b052aad
aline-camargo <aline97camargo@gmail.com>
2019-11-18 15:09:12 -0300

    layout card ok

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,20 +1,25 @@
 const cardsTemplate = (event) => {
   const template = `
-    <article class="card" style="width: 18rem;">
-        <div class="d-flex justify-content-between">
+    <article class="card card-size p-1 cards-background">
+        <div class="d-flex justify-content-between p-2 bg-white">
             <div>
-                Avaliações
+                <p class="font-regular-size m-0 font-weight-bold">Avaliações:</p>
             </div>
             <div>
-                <i class="fas fa-share-alt"></i>
-                <i class="far fa-bookmark"></i>
+                <i class="far fa-bookmark icon-regular-size"></i>
             </div>
         </div>
-        <img class="card-img-top" src=${event.img} alt="Imagem de capa do card">
-        <div class="card-body p-2">
-            <h5 class="card-title text-truncate font-weight-bold font-regular-size">${event.title}</h5>
-            <p class="card-text font-regular-size">${event.date} - ${event.hour}</p>
-            <p class="card-text text-truncate font-regular-size"><i class="fas fa-map-marker-alt"></i> ${event.local}</p>
+        <img class="img-card my-1" src=${event.img} alt="Imagem de capa do card">
+        <div class="card-body p-2 d-flex bg-white justify-content-between">
+            <div class="w-75">
+                <h5 class="card-title text-truncate font-weight-bold font-regular-size mb-1">${event.title}</h5>
+                <p class="card-text font-regular-size mb-1">${event.date} - ${event.hour}</p>
+                <p class="card-text text-truncate font-regular-size m-0"><i class="fas fa-map-marker-alt icon-small-size"></i> ${event.local}</p>
+            </div>
+            <div class="d-flex align-items-center flex-column justify-content-around">
+                <i class="fas fa-plus icon-regular-size"></i>
+                <p class="font-small-size text-center m-0">Saiba Mais</p>
+            </div>
         </div>
     </article>
     `;
