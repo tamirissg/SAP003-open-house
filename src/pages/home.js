7ddfdbe95e8aba1290b22c990893d0eb4f927df9
aline-camargo <aline97camargo@gmail.com>
2019-11-18 14:26:18 -0300

    initial card

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,35 @@
+const cardsTemplate = (event) => {
+  const template = `
+    <article class="card" style="width: 18rem;">
+        <div class="d-flex justify-content-between">
+            <div>
+                Avaliações
+            </div>
+            <div>
+                <i class="fas fa-share-alt"></i>
+                <i class="far fa-bookmark"></i>
+            </div>
+        </div>
+        <img class="card-img-top" src=${event.img} alt="Imagem de capa do card">
+        <div class="card-body p-2">
+            <h5 class="card-title text-truncate font-weight-bold font-regular-size">${event.title}</h5>
+            <p class="card-text font-regular-size">${event.date} - ${event.hour}</p>
+            <p class="card-text text-truncate font-regular-size"><i class="fas fa-map-marker-alt"></i> ${event.local}</p>
+        </div>
+    </article>
+    `;
+
+  return template;
+};
+
 const getEvents = () => {
   firebase.firestore().collection('events')
     .get()
     .then((querySnapshot) => {
       querySnapshot.forEach((doc) => {
-        console.log(doc.data());
+        document.querySelector('main').innerHTML = cardsTemplate(doc.data());
       });
     });
 };
-
-const cardsTemplate = () => {};
 
 export default getEvents;
