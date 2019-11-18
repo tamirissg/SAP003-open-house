97a33733528f179ec67dea3ef64a0863cd14f48c
Monica Yamazaki <monicasayama@gmail.com>
2019-11-18 18:52:39 -0300

    importado o login via google e fb no modal

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 import getEvents from './pages/home.js';
+import loginGoogle from '/pages/google.js';
+import loginFacebook from'/pages/facebook.js';
 
 getEvents();
 
 const checkUser = () => {
   console.log('clicou');
-  if ( firebase.auth().currentUser == null) {
+  if (firebase.auth().currentUser == null) {
     $('#myModal').modal('show');
-    $('#myModal').on('hidden.bs.modal', (e) => {
-      console.log('close modal')
-      
-    });
+    // $('#myModal').on('hidden.bs.modal', (e) => {
+    //   console.log('close modal');  
+    // });
   } else {
     console.log('go to profile');
   }
@@ -20,3 +21,8 @@ const userTop = document.querySelector('.nav-user-top');
 const userBot = document.querySelector('.nav-user-bot');
 userTop.addEventListener('click', checkUser);
 userBot.addEventListener('click', checkUser);
+
+const googleBtn = document.querySelector('.google-login');
+const facebookBtn = document.querySelector('.facebook-login');
+googleBtn.addEventListener('click', loginGoogle);
+facebookBtn.addEventListener('click', loginFacebook);
