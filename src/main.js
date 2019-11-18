8bae3084036314980ae2839338c5bb193c37241b
rosi-souza <rosi.souza@live.com>
2019-11-18 18:37:23 -0300

    criando perfil do usuario

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
-import getEvents from './pages/home.js';
+// import getEvents from './pages/home.js';
+import profile from './pages/profile.js';
 
-getEvents();
+// getEvents();
+
+function init() {
+  document.querySelector('body').innerHTML = profile();
+}
+
+window.addEventListener('load', init);
