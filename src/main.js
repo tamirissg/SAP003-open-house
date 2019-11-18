28866630a7ebbc127f3229849c723a9b0f4cf6ca
Monica Yamazaki <monicasayama@gmail.com>
2019-11-18 18:33:13 -0300

    add modal no btn perfil

--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
 import getEvents from './pages/home.js';
 
 getEvents();
+
+const checkUser = () => {
+  console.log('clicou');
+  if ( firebase.auth().currentUser == null) {
+    $('#myModal').modal('show');
+    $('#myModal').on('hidden.bs.modal', (e) => {
+      console.log('close modal')
+      
+    });
+  } else {
+    console.log('go to profile');
+  }
+
+}
+
+const userTop = document.querySelector('.nav-user-top');
+const userBot = document.querySelector('.nav-user-bot');
+userTop.addEventListener('click', checkUser);
+userBot.addEventListener('click', checkUser);
