5c99693434e8328371a674ee159811feac5b454e
Monica Yamazaki <monicasayama@gmail.com>
2019-11-19 10:28:42 -0300

    login pelo google salvando usuario

--- src/main.js
+++ src/main.js
@@ -1,21 +1,34 @@
 import getEvents from './pages/home.js';
-import loginGoogle from '/pages/google.js';
-import loginFacebook from'/pages/facebook.js';
+import loginGoogle from './pages/google.js';
+import loginFacebook from './pages/facebook.js';
 
 getEvents();
 
 const checkUser = () => {
-  console.log('clicou');
   if (firebase.auth().currentUser == null) {
     $('#myModal').modal('show');
     // $('#myModal').on('hidden.bs.modal', (e) => {
-    //   console.log('close modal');  
+    //   console.log('close modal');
     // });
   } else {
     console.log('go to profile');
   }
+};
 
-}
+const signIn = () => {
+  const email = document.querySelector('.input-email-login').value;
+  const password = document.querySelector('.input-password-login').value;
+  firebase.auth().signInWithEmailAndPassword(email, password).then(() => {
+    $('#myModal').modal('hide');
+  }).catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = document.querySelector('.error');
+    if (errorCode === 'auth/invalid-email') errorMessage.textContent = 'Email inválido';
+    if (errorCode === 'auth/user-disabled') errorMessage.textContent = 'Usuário desabilitado';
+    if (errorCode === 'auth/user-not-found') errorMessage.textContent = 'Usuário não encontrado';
+    if (errorCode === 'auth/wrong-password') errorMessage.textContent = 'Senha incorreta';
+  });
+};
 
 const userTop = document.querySelector('.nav-user-top');
 const userBot = document.querySelector('.nav-user-bot');
@@ -26,3 +39,6 @@ const googleBtn = document.querySelector('.google-login');
 const facebookBtn = document.querySelector('.facebook-login');
 googleBtn.addEventListener('click', loginGoogle);
 facebookBtn.addEventListener('click', loginFacebook);
+
+const loginBtn = document.querySelector('.btn-submit-login');
+loginBtn.addEventListener('click', signIn);
--- src/pages/google.js
+++ src/pages/google.js
@@ -1,12 +1,23 @@
 function loginGoogle() {
   const provider = new firebase.auth.GoogleAuthProvider();
   firebase.auth().signInWithPopup(provider)
-    .then((result) => {
-
-
-    }).catch((error) => {
-      alert('Falha na autenticação');
+    .then((currentUser) => {
+      const usersCollection = firebase.firestore().collection('users');
+      usersCollection.where('user_uid', '==', currentUser.user.uid).get()
+        .then((snap) => {
+          if (snap.size === 0) {
+            const user = {
+              nome: currentUser.additionalUserInfo.profile.given_name,
+              sobrenome: currentUser.additionalUserInfo.profile.family_name,
+              user_uid: currentUser.user.uid,
+            };
+            firebase.firestore().collection('users').add(user);
+          }
+          $('#myModal').modal('hide');
+        }).catch((error) => {
+          alert('Falha na autenticação');
+        });
     });
-}
+};
 
 export default loginGoogle;
