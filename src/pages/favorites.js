51fafa61553b9997c985093fa5f5a978fd6396ae
aline-camargo <aline97camargo@gmail.com>
2019-11-20 10:58:19 -0300

    css cards mobile ok

--- src/pages/favorites.js
+++ src/pages/favorites.js
@@ -1,10 +1,17 @@
+import funcs from './home.js';
+
 const favoritesTemplate = (props) => {
   const template = `
-  <article class="card d-flex flex-row favorite-card-width">
+  <article class="card d-flex flex-row favorite-card p-1 cards-background">
     <img src=${props.img} class="favorite-card-img">
-    <div class="card-body favorite-card-body">
-    <h5 class="card-title text-truncate font-weight-bold font-regular-size mb-1">${props.title}</h5>
-    <p class="card-text font-regular-size mb-1">${props.date} - ${props.hour}</p>
+    <div class="p-2 favorite-card-body bg-white ml-1">
+      <h5 class="card-title text-truncate font-weight-bold font-regular-size mb-1">${props.title}</h5>
+      <p class="card-text font-regular-size mb-1">${props.date} - ${props.hour}</p>
+      <p class="card-text text-truncate font-regular-size m-0"><i class="fas fa-map-marker-alt icon-small-size"></i> ${props.local}</p>
+      <div class="d-flex align-items-center flex-row justify-content-center mt-2" id=${props.id} onClick="a.handleClick(event,${funcs.moreInfo})">
+        <i class="fas fa-plus icon-regular-size"></i>
+        <p class="text-center mb-0 ml-1"> Saiba Mais</p>
+      </div>
     </div>
 </article>
     `;
@@ -24,7 +31,11 @@ const getFavorites = () => {
             .doc(ref)
             .get()
             .then((querySnapshot2) => {
-              document.querySelector('main').innerHTML = favoritesTemplate(querySnapshot2.data());
+              const docEvent = {
+                ...querySnapshot2.data(),
+                id: doc.id,
+              }
+              document.querySelector('main').innerHTML = favoritesTemplate(docEvent);
             });
         });
       });
