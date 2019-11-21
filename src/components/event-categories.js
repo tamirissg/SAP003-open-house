b577a4c7ab47063e371cb2a6f290f909f702812d
GeniferGonzalezSantos <genh_@hotmail.com>
2019-11-20 21:04:12 -0300

    ajustar categorias no desktop

--- src/components/event-categories.js
+++ src/components/event-categories.js
@@ -1,7 +1,7 @@
 const templateCategory = (props) => {
   const template = `
     <div class="div-category">
-        <button class="b-category" >
+        <button class="b-category" Id='category' onclick="a.handleClick(event, ${props.onClick})">
             <img src=${props.src} alt="" class="img-category">
         </button>
            <a href="" class="a-category"><p class="p-category">${props.title}</p></a>
